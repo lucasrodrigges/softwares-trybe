f854872f0f04b7c4945b91eab1a90636086a7880
Lucas Rodrigues <99984705+lucas-rodrigges@users.noreply.github.com>
2022-04-08 00:31:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,3 +58,5 @@ echo "Tudo protno, reinicie sua máquina."
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm -v npm -v node -v git --version
