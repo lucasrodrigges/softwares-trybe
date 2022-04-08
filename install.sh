1a7c1b778773b01b88208be0ce2d4c3be671382f
Lucas Rodrigues <99984705+lucas-rodrigges@users.noreply.github.com>
2022-04-07 23:56:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,3 +55,6 @@ flatpak install flathub com.slack.Slack
 
 
 echo "Tudo protno, reinicie sua máquina."
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
