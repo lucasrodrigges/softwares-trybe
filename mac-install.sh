7dafde38487e8153bd44735692046c0510817fc5
Lucas Rodrigues <99984705+lucas-rodrigges@users.noreply.github.com>
2022-04-10 01:42:25 -0300

    Update mac-install.sh

--- mac-install.sh
+++ mac-install.sh
@@ -1,4 +1,5 @@
 ## Installing nvm ##
+
 touch ~/.zshrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
