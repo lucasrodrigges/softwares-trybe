983c75c197a3e25f2da0afddbd616af60ee1adaf
Lucas Rodrigues <99984705+lucas-rodrigges@users.noreply.github.com>
2022-04-12 17:30:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,10 @@
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 
-
 ## Updating System ##
 
 sudo apt update 
 sudo apt upgrade -y
-
 
 ## Google Chrome ##
 
@@ -24,7 +22,6 @@ sudo dpkg -i *.deb
 
 sudo apt install code
 
-
 ## Flatpak Support##
 
 sudo apt install flatpak -y
@@ -34,15 +31,8 @@ sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
 
-
 ## Apps Flatpak ##
 
 flatpak install flathub us.zoom.Zoom -y
 flatpak install flathub io.github.shiftey.Desktop -y
 flatpak install flathub com.slack.Slack -y
-
-## Checking Installation ##
-
-
-echo "Tudo protno, reinicie sua máquina."
-
