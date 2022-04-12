d64898bf89d93cdbdb145dd3b0584a4309b9eef1
Lucas Rodrigues <99984705+lucas-rodrigges@users.noreply.github.com>
2022-04-12 17:30:49 -0300

    Update nvm-install.sh

--- nvm-install.sh
+++ nvm-install.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 ## Installing NodeJS LTS ##
-
 sudo apt install npm -y
 sudo apt install curl 
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 nvm install --lts
 
 ## Installing Git ##
-
 sudo apt-get install git -y
 sudo add-apt-repository ppa:git-core/ppa -y
 
-exit
+echo "Tudo protno, reinicie sua máquina."
