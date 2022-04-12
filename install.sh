a47467d4a82d9cc9afabadac020ef2e75501e915
Lucas Rodrigues <99984705+lucas-rodrigges@users.noreply.github.com>
2022-04-12 17:19:02 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,22 +21,6 @@ wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.de
 sudo dpkg -i *.deb
 
 
-## Installing NodeJS LTS ##
-
-sudo apt install npm -y
-sudo apt install curl 
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-nvm install --lts
-
-## Installing Git ##
-
-sudo apt-get install git -y
-sudo add-apt-repository ppa:git-core/ppa -y
-
-## Installing VSCode ##
-
-sudo apt install code
-
 ## Flatpak Support##
 
 sudo apt install flatpak -y
