a618a61348700a8f4385ffce8fe49f40a258ff2f
Lucas Rodrigues <99984705+lucas-rodrigges@users.noreply.github.com>
2022-04-12 17:20:38 -0300

    Add files via upload

---
+++ npm-install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+## Removing occasional crashes from apt ##
+
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
+
+## Installing NodeJS LTS ##
+
+sudo apt install npm -y
+sudo apt install curl 
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+nvm install --lts
+
+## Installing Git ##
+
+sudo apt-get install git -y
+sudo add-apt-repository ppa:git-core/ppa -y
+
+## Installing VSCode ##
+
+sudo apt install code
