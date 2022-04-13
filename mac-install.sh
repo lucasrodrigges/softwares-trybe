ae234bf7eabc257be0123df52cdb84b38a5c1c29
Lucas Rodrigues <99984705+lucas-rodrigges@users.noreply.github.com>
2022-04-13 12:09:28 -0700

    Update mac-install.sh

--- mac-install.sh
+++ mac-install.sh
@@ -1,12 +1,3 @@
-## Installing nvm ##
-touch ~/.zshrc
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install --lts
-
-
-
 ## Brew Apps ##
 
 brew install --cask google-chrome
@@ -14,6 +5,3 @@ brew install --cask slack
 brew install --cask zoom
 brew install --cask visual-studio-code
 brew install git
-
-## Checking Apps Version ##
-node -v && nvm -v && npm -v && git --version
