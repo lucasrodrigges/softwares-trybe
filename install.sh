5f181f013c9576fda69bd0bf97882a5a8174b82c
Lucas Rodrigues <99984705+lucas-rodrigges@users.noreply.github.com>
2022-04-14 00:46:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 
-## Google Chrome ##
+## Google Chrome e Zoom ##
 
 mkdir /home/$USER/Downloads/Programas
 cd /home/$USER/Downloads/Programas 
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
+wget https://zoom.us/client/latest/zoom_amd64.deb -O zoom.deb
 sudo dpkg -i *.deb
 
 ## VSCode ##
@@ -24,14 +25,3 @@ sudo apt install code
 
 ## Slack ##
 sudo apt install slack-desktop 
-
-## Flatpak Support##
-sudo apt install flatpak -y
-sudo add-apt-repository ppa:flatpak/stable -y
-sudo apt update -y
-sudo apt install flatpak -y
-sudo apt install gnome-software-plugin-flatpak -y 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
-
-## Apps Flatpak ##
-flatpak install flathub us.zoom.Zoom -y
