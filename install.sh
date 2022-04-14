05139c8cfe854121702fa8127720871c54e1f07b
Lucas Rodrigues <99984705+lucas-rodrigges@users.noreply.github.com>
2022-04-14 01:04:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,18 @@
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 
-## Google Chrome e Zoom ##
+## Google Chrome ##
 
 mkdir /home/$USER/Downloads/Programas
 cd /home/$USER/Downloads/Programas 
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
-wget https://zoom.us/client/latest/zoom_amd64.deb -O zoom.deb
 sudo dpkg -i *.deb
+
+## Zoom ##
+wget https://zoom.us/client/latest/zoom_amd64.deb -O zoom.deb
+sudo dpkg -i zoom.deb
+sudo apt-get install -f
+sudo apt install ./zoom.deb
 
 ## VSCode ##
 sudo apt-get install wget gpg
